ded4b07674c57d4cda173c623dc0e09d51731ec4
leandro_correa <leandrocorreati@gmail.com>
2018-10-18 23:02:27 -0300

    Aula 02

---
+++ src/Aula02/Aula02.java
@@ -0,0 +1,14 @@
+package Aula02;
+
+public class Aula02
+{
+    private String privado;
+    protected  String protegido;
+    public  String publico;
+    String padrao;
+
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/Aula02/Principal.java
@@ -0,0 +1,15 @@
+package Aula02;
+
+public class Principal
+{
+    public static void main(String[] args)
+    {
+        Aula02 a = new Aula02();
+        a.padrao = "Padrão";
+        a.protegido = "Protegido";
+        a.publico = "Publico";
+        // v v v v v f f v f f
+        // v v v f  f f f f v f
+
+    }
+}
