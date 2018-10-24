84818589f4756d03647c8b0300ee1895154662ac
leandro_correa <leandrocorreati@gmail.com>
2018-10-23 22:09:48 -0300

    Aula 04

--- src/Aula04/Exercicio.java
+++ src/Aula04/Exercicio.java
@@ -2,5 +2,59 @@ package Aula04;
 
 public class Exercicio
 {
+    private  String nome;
+    private String  sobrenome;
+    private int idade;
+    private char sexo;
 
+    public Exercicio()
+    {
+        super();
+    }
+
+    public Exercicio(String nome, String sobrenome, int idade) {
+        super();
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+    }
+
+    public Exercicio(char sexo) {
+        super();
+        this.sexo = sexo;
+    }
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
 }
