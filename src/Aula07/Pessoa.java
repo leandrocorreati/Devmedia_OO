c863c683add1798ad57bf1df84215a6ce1f2e9ff
leandro_correa <leandrocorreati@gmail.com>
2018-10-24 21:49:39 -0300

    Aula06

---
+++ src/Aula07/Pessoa.java
@@ -0,0 +1,36 @@
+package Aula07;
+
+public class Pessoa
+{
+    private String nome;
+    private String matricula;
+
+    public Pessoa(String nome, String matricula)
+    {
+        super();
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa: " + " nome = '" + nome + '\'' + ", matricula = '" + matricula ;
+    }
+}
---
+++ src/Aula07/Principal.java
@@ -0,0 +1,11 @@
+package Aula07;
+
+public class Principal
+{
+    public static void main(String[] args)
+    {
+        Aluno a = new Aluno("Leandro","45678");
+        System.out.println(a.toString());
+    }
+
+}
